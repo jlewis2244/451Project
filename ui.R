6c3778685b98ce7bda9a473a51613e03eebd0209
jlewis2244 <101019864+jlewis2244@users.noreply.github.com>
2023-11-30 15:48:54 -0800

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,8 +5,8 @@ ui <- fluidPage(
       sliderInput(
         inputId = "year",
         label = "Year of WC",
-        min = min(Match$Year),
-        max = max(Match$Year),
+        min = 1930,
+        max = 2014,
         value = 2014,
         step = 4,
         sep = "",
