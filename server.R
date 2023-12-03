14d80ab4266926337b1ad07956dd8f7c0c608666
jlewis2244 <101019864+jlewis2244@users.noreply.github.com>
2023-12-03 15:00:57 -0800

    Update server.R

--- server.R
+++ server.R
@@ -22,7 +22,7 @@ server <- function(input, output) {
       group_by(Total.Goals) %>%
       summarize(Average_Attendance = mean(Attendance), Count = n())
     
-    ggplot(Average_Attendance, aes(x = Total.Goals, y = Average_Attendance, fill = Count)) +
+    ggplot(Average_Attendance, aes(x = Total.Goals, y = Average_Attendance, size = Count)) +
       geom_bar(stat = "identity") +
       scale_fill_gradient(low = "lightblue", high = "darkblue") +
       labs(title = "Average Attendance for Each Number of Goals",
